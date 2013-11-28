dbe14dd864654fc9cc7037a5db1022e7ae5b3b23
Stuart Branham <stuart.branham@yourcls.com>
2013-11-28 16:08:04 -0500

    initial commit

---
+++ Dragonputer/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Dragonputer.WebApiApplication" Language="C#" %>
---
+++ Dragonputer/Scripts/app/dragonputer.js
@@ -0,0 +1,13 @@
+﻿var dragonputer = angular.module('dragonputer', []);
+
+/*
+dragonputer.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            controller: 'CharacterController',
+            templateUrl: '',
+
+        })
+        .otherwise({ redirectTo: '/' });
+});
+*/
