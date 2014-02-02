ff5af4f2ef47eb58e23eef3b83596b2afc435d0d
Stuart Branham <stuart.branham@gmail.com>
2014-02-02 15:20:34 -0500

    variable background opacity

--- Dragonputer/Scripts/app/cs-background-image.js
+++ Dragonputer/Scripts/app/cs-background-image.js
@@ -9,17 +9,44 @@
         return {
             restrict: 'A',
             scope: {
-                image: '=csBackgroundImage'
+                image: '=csBackgroundImage',
             },
             link: function (scope, $element, attrs) {
-                scope.$watch('image', function () {
-                    var data = scope.image
+                function refresh() {
+                    var imageData = scope.image
                         ? 'url(' + scope.image + ')'
                         : 'none';
                     $element.css({
-                        'background-image': data
+                        'background-image': imageData
                     });
-                });
+                }
+                scope.$watch('image', refresh);
+            }
+        };
+    });
+    angular.module('dragonputer').directive('csBackgroundOpacity', function () {
+        return {
+            restrict: 'A',
+            scope: {
+                opacity: '=csBackgroundOpacity'
+            },
+            link: function (scope, $element, attrs) {
+                function refresh() {
+                    var color = $element.css('background-color');
+                    var value = scope.opacity / 100.0;
+                    if (color.match(/^rgba\(/)) {
+                        color = color.replace(/[^,]+\)$/, value.toString() + ')');
+                    }
+                    else if (color.match(/^rgb\(/)) {
+                        color = color.replace(/^rgb\(/, 'rgba(');
+                        color = color.replace(/\)$/, ',' + value.toString() + ')');
+                    }
+                    console.log('color', color);
+                    $element.css({
+                        'background-color': color
+                    });
+                }
+                scope.$watch('opacity', refresh);
             }
         };
     });
